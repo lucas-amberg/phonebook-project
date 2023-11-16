c4aea8fcfee426a655c17a2a87d72a5ed4f2db7f
lucas-amberg <102396588+lucas-amberg@users.noreply.github.com>
2023-11-16 11:58:21 -0800

    Add functionality for deleting persons (3.4)

--- index.js
+++ index.js
@@ -47,6 +47,12 @@ app.get("/info", (request, response) => {
     `)
 })
 
+app.delete("/api/persons/:id", (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
 
 
 const PORT = 3001
