2a0ca8076cbac13ccea6ff271cd7255075c78ddd
lucas-amberg <102396588+lucas-amberg@users.noreply.github.com>
2023-11-23 14:09:00 -0800

    Link backend to database (3.13-3.14)

--- index.js
+++ index.js
@@ -1,44 +1,49 @@
+require("dotenv").config()
+
 const express = require("express")
 const morgan = require("morgan")
 const cors  =  require("cors")
+const Person = require("./models/person.js")
 
 morgan.token("data", (request) => {
     return JSON.stringify(request.body)
 })
 
-const app = express()
-app.use(express.static("dist"))
+const app = express() //Sets app to express
+app.use(express.static("dist")) //Sets frontend to dist directory
 
 
 app.use(express.json())
 app.use(cors())
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :data"))
 
-// Initial persons
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
-let id = persons.length
+// OLD OG LIST OF NUMBERS AND NAMES
+// Initial persons
+// let persons = [
+//     { 
+//       "id": 1,
+//       "name": "Arto Hellas", 
+//       "number": "040-123456"
+//     },
+//     { 
+//       "id": 2,
+//       "name": "Ada Lovelace", 
+//       "number": "39-44-5323523"
+//     },
+//     { 
+//       "id": 3,
+//       "name": "Dan Abramov", 
+//       "number": "12-43-234345"
+//     },
+//     { 
+//       "id": 4,
+//       "name": "Mary Poppendieck", 
+//       "number": "39-23-6423122"
+//     }
+// ]
+
+// let id = persons.length
 
 // Function to generate new ID between 0 and 999 for new person
 const newId = () => {
@@ -50,13 +55,16 @@ const newId = () => {
 
 // Shows all persons
 app.get("/api/persons", (request, response) => {
-    response.send(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 // Shows individual person RESTfully by searching their ID
 app.get("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id)
-    response.send(persons.find(person => person.id === id))
+    Person.findById(request.params.id).then(person => {
+        response.json(person)
+    })
 })
 
 // Displays information about the API
@@ -82,6 +90,7 @@ app.delete("/api/persons/:id", (request, response) => {
 // Adds a new person with a post request to /api/persons
 app.post("/api/persons", (request, response) => {
     const body = request.body
+    console.log(request.body)
     if (!body.name) {
         return response.status(400).json({
             error: "name missing"
@@ -92,23 +101,24 @@ app.post("/api/persons", (request, response) => {
             error: "number missing"
         })
     }
-    else if (persons.find(person => person.name === body.name)) {
-        return response.status(400).json({
-            error: "name must be unique"
-        })
-    }
-    const newPerson = {
-        id: newId(),
+    // else if (Person.find({name: body.name})) {
+    //     return response.status(400).json({
+    //         error: "name must be unique"
+    //     })
+    // }
+    const newPerson = new Person({
         name: body.name,
         number: body.number
-    }
-    persons = persons.concat(newPerson)
+    })
+    
+    newPerson.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
 
-    response.json(newPerson)
 })
 // Starts server
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
---
+++ models/person.js
@@ -0,0 +1,34 @@
+require("dotenv").config()
+
+const mongoose = require("mongoose")
+
+mongoose.set("strictQuery", false)
+
+const url = process.env.MONGODB_URL
+
+console.log("connecting to mongodb+srv://mongo-access:<PASSWORD>@phonebook-cluster.b014m5d.mongodb.net/phonebookApp?retryWrites=true&w=majority")
+
+mongoose.connect(url) //Connects to database
+    .then(result => {
+        console.log("connected to MongoDB")
+    })
+    .catch(error => {
+        console.log("error connecting to MongoDB, error: ", error.message)
+    })
+
+//Template for Person Document in Mongo Cluster
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+//Makes it so the API does not display a __v and _id attribute because they are unnessessary
+personSchema.set("toJSON", {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model("Person", personSchema)
